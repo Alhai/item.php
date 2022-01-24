e1b3d31050780728e078d0d22bc64793ad3c59c1
Aly HAIDARA <aly.haidara@Digital-Grenoble.local>
2022-01-24 14:18:09 +0100

    second commit

---
+++ item.php
@@ -0,0 +1,4 @@
+<?php
+$Nom = "Le chatcasque";
+$Prix = "30$";
+$Url = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.ldlc.com%2Ffiche%2FPB00336045.html&psig=AOvVaw0v9oqTl5O5cc2CEyJZg6jt&ust=1643115613460000&source=images&cd=vfe&ved=0CAgQjRxqFwoTCIj2vbS4yvUCFQAAAAAdAAAAABAK";
