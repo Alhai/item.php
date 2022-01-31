853cdb8a1600bde14bca2b211759c0eeb730fd50
Aly HAIDARA <aly.haidara@Digital-Grenoble.local>
2022-01-31 16:55:11 +0100

    format prix tableau simple

--- catalog-with-keys.php
+++ catalog-with-keys.php
@@ -1,7 +1,7 @@
 <?php
 $chatcasqueRose= [
     "Nom" => "Le chatcasque Rose",
-    "Prix" => "30$",
+    "Prix" => 3275,
     "Poids" => "200",
     "discount" => 10,
     "Url" => "https://www.cdiscount.com/pdt2/1/4/7/3/700x700/hom0753308545147/rw/casque-d-oreille-chat-casque-sans-fil-pliable-casq.jpg",
@@ -9,7 +9,7 @@ $chatcasqueRose= [
 
 $chatcasqueViolet = [
     "Nom" => "Le chatcasque Violet",
-    "Prix" => 34,
+    "Prix" => 3499,
     "Poids" => 200,
     "discount" => 3,
     "Url" => "https://www.cdiscount.com/pdt2/2/1/2/1/700x700/one0761276024212/rw/sans-fil-bluetooth-5-0-casque-led-lumiere-garcon-e.jpg",];
@@ -20,9 +20,12 @@ $chatcasqueViolet = [
             if ($Caracteristique === "Url"){
               echo "<img src=" . $chatcasqueRose["Url"] . " alt= image d'un casque de chat rose>" ;
             }
+            elseif ($Caracteristique === "Prix"){
+              echo $Caracteristique . ":" . $value/100 .' €';
+            }
             else{
               echo $Caracteristique . ":" . $value . "<br>" ;
-            }
+            } 
 
 
         }
@@ -32,11 +35,14 @@ $chatcasqueViolet = [
             if ($Caracteristique2 === "Url"){
               echo "<img src=" . $chatcasqueViolet["Url"] . " alt= image d'un casque de chat rose>" ;
             }
+            elseif ($Caracteristique2 === "Prix"){
+              echo $Caracteristique2 . ":" . $value2/100 .' €' . "\n";
+            }
             else{
               echo $Caracteristique2 . ":" . $value2 . "<br>" ;
             }
 
 
-      }
-       
+      }    
+      
 ?>
