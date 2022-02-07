9a7fdb8f034224c6fc986786f6f40ce6fc8a8d2b
Aly Haidara <alhai.ynw@gmail.com>
2022-02-07 01:42:59 +0100

    card

---
+++ card.php
@@ -0,0 +1,51 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="./images_casques\airpods-max-select-silver-202011.png" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">AirPods Max </h5>
+    <p class="card-text">Voici les AirPods Max. L’accord parfait entre un son haute fidélité enivrant et la magie intuitive des AirPods. Préparez‑vous pour une expérience d’écoute inouïe.</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Colorie : Gris </li>
+    <li class="list-group-item">600€</li>
+    <li class="list-group-item">Réduction</li>
+  </ul>
+</div>
+
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="./images_casques\casque_MontBlanc.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Casque over-ear Montblanc MB 01</h5>
+    <p class="card-text">Le casque over-ear Montblanc MB 01 est conçu pour répondre à la demande croissante de nos clients à la recherche de plus de confort lors de réunions prolongées, de qualité sonore et d'expérience haptique.</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Coloris : Noir</li>
+    <li class="list-group-item">600 €</li>
+    <li class="list-group-item">Réduction </li>
+  </ul>
+</div>
+
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="./images_casques\dt-770-pro-250-ohms-hd-2-50268.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">BeyerDynamic DT-770 Pro 80 Ohm </h5>
+    <p class="card-text">Le Beyerdynamic DT 770 Pro 80 est un casque dynamique fermé, conçu pour le contrôle et le monitoring avec une impédance de 80 ohms. Il est depuis toujours un accessoire essentiel dans les studios du monde entier.</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Coloris : Noir</li>
+    <li class="list-group-item">Dapibus ac facilisis in</li>
+    <li class="list-group-item">Vestibulum at eros</li>
+  </ul>
+</div>
+</body>
+</html>
+
